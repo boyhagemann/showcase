38e24cd40e850e0215ae939ee7d57ba3640c5ea7
Boy Hagemann <bh@web1on1.nl>
2017-06-24 00:10:36 +0200

    Added baseurl in .env

--- src/components/UI/Workspace.js
+++ src/components/UI/Workspace.js
@@ -24,7 +24,7 @@ const logoutAndRedirect = logout => {
 }
 
 export default ({ isAuthenticated, logout }) => (
-  <ConnectedRouter history={history}>
+  <ConnectedRouter history={history} basename={process.env.BASE_URL}>
       <Content>
         <NavbarContainer />
         <Route exact path="/" component={HomeContainer} />
